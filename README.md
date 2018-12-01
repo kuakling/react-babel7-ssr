f5094fd2133335b8daef68371df9a44b5d9dc3c9
Surakit Choodet <kuakling@gmail.com>
2018-12-01 09:53:46 +0700

    Initial Project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # react-babel7-ssr
+React 16, Webpack 4, Babel 7 and Server side rendering
---
+++ src/shared/core/config.js
@@ -0,0 +1,4 @@
+export default {
+  appContainerId: 'react-root',
+  baseUrl: process.env.REACT_APP_BASE_URL ? `/${process.env.REACT_APP_BASE_URL}` : '',
+}
